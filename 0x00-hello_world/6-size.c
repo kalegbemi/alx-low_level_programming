303dbad94d511f077a49585e2d8d17a53c2ba5a6
kayode james alegbemi <kayode.alegbemi@gmail.com>
2023-08-10 18:36:17 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,11 @@
  */
 int main(void)
 
+{	
 	int i;
 	double d;
 	char c;
-{
+
 	printf("the size of an int is : %lu.\n", (unsigned long)sizeof(i));
 	printf("the size of a double is : %lu.\n", (unsigned long)sizeof(d));
 	printf("the size of a char is : %lu. \n", (unsigned long)sizeof(c));
