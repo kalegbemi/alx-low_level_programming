b12a1a0e17c00cc9b7d00a229b86158c266d308e
kayode james alegbemi <kayode.alegbemi@gmail.com>
2023-08-10 19:09:20 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,15 @@
 int main(void)
 {
 	int i;
-	double d;
 	char c;
 	long int l;
 	long long int e;
 	float f;
 
 	printf("size of an int : %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("size of a double : %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("size of a char : %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
 	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(e));
-	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
