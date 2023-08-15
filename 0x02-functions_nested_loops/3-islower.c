91a410ad5ffaeb48008857cddd450d3137e78228
kayode james alegbemi <kayode.alegbemi@gmail.com>
2023-08-15 16:52:20 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,14 @@
 /**
  * _islower - Checks for lowercase alphabet
  * Description: to check if c is lower case
- * Return: if character is lowercase 0 , otherwise 1
+ * Return: if character is lowercase 1 , otherwise 0
  */
 
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return (0);
-	else
 		return (1);
+	else
+		return (0);
 }
 
