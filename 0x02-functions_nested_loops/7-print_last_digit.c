78a7b7e613f274163b6a3ab4c083fcb11c387487
kayode james alegbemi <kayode.alegbemi@gmail.com>
2023-08-15 19:16:43 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,18 @@
 #include "main.h"
+/**
+ * print_last_digit - to print the last digit of a number
+ * @a: function parameter
+ * Return: Always 0
+ */
 
-
-int print_last_digit(int)
+int print_last_digit(int a)
 {
+	int b = a % 10;
+
+	if (a < 0)
+		b = -b;
+
+	_putchar(b + '0');
+	return (b);
+}
 
