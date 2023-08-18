f19ebbafda840e1a3a27c1c27622dfedce1c4af0
kayode james alegbemi <kayode.alegbemi@gmail.com>
2023-08-17 17:41:48 -0700

    print numbers in 10 columns

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - print arrays of numbers
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 1; a < 11; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 9)
+				_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
