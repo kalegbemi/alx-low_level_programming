5f4d251f33d175364d2652c39cb03dced402ea72
kayode james alegbemi <kayode.alegbemi@gmail.com>
2023-08-17 17:56:07 -0700

    draw a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - print line using '_'
+ * @n: function parameter
+ * Return: o
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int a;
+
+		for (a = 1; a <= n; a++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
