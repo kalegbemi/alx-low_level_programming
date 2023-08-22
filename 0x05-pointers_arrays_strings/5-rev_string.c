581edc6e11b5d0c8acea5a190c38a6b1b3a0de3a
kayode james alegbemi <kayode.alegbemi@gmail.com>
2023-08-21 18:21:28 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * re_string - function for rev string
+ * @s: the string
+ */
+void rev_string(char *s)
+{
+	int a;
+	int count = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+		count++;
+
+	for (a = 0; a < count / 2; a++)
+	{
+		int b;
+
+		b = s[a];
+		s[a] = s[count - 1 - a];
+		s[count - 1 - a] = b;
+	}
+}
