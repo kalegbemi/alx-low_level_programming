dba6a544465bfb77a7495b5e09bcf5d6e81386d3
kayode james alegbemi <kayode.alegbemi@gmail.com>
2023-08-21 19:22:25 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * *_strcpy - function to strings from one to another
+ * @dest: string to copy to
+ * @src: string to copy from
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != 0; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
