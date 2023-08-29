d681427346d3e2bce02e20d002c1c30922c39206
kayode james alegbemi <kayode.alegbemi@gmail.com>
2023-08-28 17:56:14 -0700

    find a string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strstr - a function to locate a substring
+ * @needle: the substring to search
+ * @haystack: the string to search from
+ * Return: haystack
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *a = haystack;
+		char *b = needle;
+
+		while (*a == *b && *b != '\0')
+		{
+			a++;
+			b++;
+		}
+
+		if (*b == '\0')
+			return (haystack);
+	}
+
+	return (0);
+
+}
