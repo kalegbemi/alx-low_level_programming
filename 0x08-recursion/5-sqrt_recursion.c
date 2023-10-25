d5dd6f679f6e901c6c50f00065cdf66e1ce76449
kalegbemi <kayode.alegbemi@gmail.com>
2023-10-25 15:42:34 -0700

    final modification on 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,11 @@
-#include <main.h>
+#include "main.h"
 #include <stdio.h>
 /**
  * _puts_recursion - function to print a string
  * @n: the integer whose squaroot we are to find
  * Return: Always 0 (success)
  */
-int int _sqrt_recursion(int n)
+int _sqrt_recursion(int n)
 {
 	int x = 1;
 	if (n / (-1) == (n))
