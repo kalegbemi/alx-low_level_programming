66940e8cf537fe4e8771ced1c7e903054bf873f5
kalegbemi <kayode.alegbemi@gmail.com>
2023-10-25 18:47:32 -0700

    modified

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,24 +1,32 @@
 #include "main.h"
+#include <stdio.h>
+int _sqrt(int n, int x);
 
 /**
- * _puts_recursion - function to print a string
+ * _sqrt_recursion - function to get the squareroot of a number
  * @n: the integer whose squaroot we are to find
  * Return: Always 0 (success)
  */
-int _sqrt(int n, int x);
 
 int _sqrt_recursion(int n)
 {
-	return (_sqrt(n, 1));
+	return (_sqrt(n, 0));
 }
 	
 	
+
+/**
+ * _sqrt - supporting function in finding the root of a number
+ * @n: the integer whose squaroot we are to find
+ * @x: the variable to iterate with
+ * Return: the function
+ */
 	
 	
 	int _sqrt(int n, int x)
 	{
-	x = 1;
-	if (n / (-1) == (n))
+	
+	if (n < 0)
 	{
 	    return (-1);
 	}
